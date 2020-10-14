634627267f30dcacf8c5d5912b23815ad420fba2
mervill <mervills.email@gmail.com>
2020-10-14 12:39:50 -0700

    NodeJs Server

---
+++ NodeJs/server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mustache = require('mustache')
+const dateFormat = require('dateformat')
+const schedule = require('node-schedule')
+const fs = require('fs')
+const os = require('os')
+
+const templates = {
+    index: fs.readFileSync(`${__dirname}/template/index.html`, 'utf8')
+}
+
+const graphDataLength = 2880
+
+let tempatureDataSet = []
+
+let logStream = null;
+
+const port = 8888
+const app = express()
+
+app.use('/static', express.static(__dirname + '/static'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send(mustache.render(templates.index, {
+        chartdata: tempatureDataSet
+    }))
+})
+
+app.post('/log', (req, res) => {
+    //console.log(`Got ${req.body.temp}`)
+    let newdata = {
+        //time: req.body.time,
+        //time: time++,
+        time: dateFormat(new Date(), "hh:MM:ssTT"),
+        temp: req.body.temp,
+    }
+
+    let logString = `${newdata.time},${newdata.temp}`
+    console.log(logString)
+    logStream.write(`${logString}\n`)
+
+    tempatureDataSet.push(newdata)
+    while (tempatureDataSet.length > graphDataLength)
+        tempatureDataSet.shift()
+    
+    let now = new Date()
+    res.send(`<pre>${dateFormat(now, "hh:MM:ss TT - dd/mm/yy")}</pre>`)
+})
+
+app.get('/now', (req, res) => {
+    let now = new Date()
+    res.send(`<pre>${dateFormat(now, "hh:MM:ss TT - dd/mm/yy")}</pre>`)
+})
+
+app.get('/id', (req, res) => {
+    const machineInfo = {
+        hostname: os.hostname(),
+        platform: os.platform(),
+        arch: os.arch(),
+        uptime: os.uptime(),
+        loadavg: os.loadavg(),
+        cpus: os.cpus(),
+        networkInterfaces: os.networkInterfaces(),
+    }
+    res.send(`<pre>${JSON.stringify(machineInfo, undefined, 2)}</pre>`)
+})
+
+function SetLogStream()
+{
+    if (logStream != null && logStream.writeable)
+    {
+        logStream.end()
+    }
+
+    let dateCode = dateFormat(new Date(),"dd-mm-yy")
+    let logName = `log-${dateCode}.txt`
+    logStream = fs.createWriteStream(logName, {flags:'a'})
+    
+    logStream.write(`${dateFormat(new Date(),"dd/mm/yy @ hh:MM:ssTT")},\n`)
+    logStream.write(`time,temperature\n`)
+
+    console.log(`Log file is now ${logName}`)
+    console.log(`Log rollover at ${dateFormat(new Date(), "hh:MM:ssTT - dd/mm/yy")}`)
+}
+
+app.listen(port, () => {
+    SetLogStream();
+    schedule.scheduleJob('0 0 * * *', () => {
+        SetLogStream();
+    })
+    console.log(`app listening on port ${port}`)
+})
