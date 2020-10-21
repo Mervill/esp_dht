651ca9713273f77d8d2402704063b27a151df07c
mervill <mervills.email@gmail.com>
2020-10-20 17:33:12 -0700

    much better log handling and small html improvements

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -19,9 +19,64 @@ const templates = {
 
 const graphDataLength = (2 * 60 * 24) // 2880
 
-let tempatureDataSet = []
+let NodeRegistry = []
 
 let logStream = null;
+
+function FindNodeData(nickname, lazyCreate)
+{
+    for (const node of NodeRegistry)
+    {
+        if (node.Nickname == nickname)
+        {
+            return node;
+        }
+    }
+
+    if (!lazyCreate)
+    {
+        console.log(`Didn't find node data for nickname ${nickname}`)    
+        return null
+    }
+        
+
+    // didn't find a node with that nickname, so create one
+    let newNodeData = {
+        Nickname: nickname,
+        DisplayGraphData: [],
+        AquireLogStream: NodeData_AquireLogStream,
+    }
+    newNodeData.AquireLogStream()
+    NodeRegistry.push(newNodeData)
+
+    console.log(`Created new node data for nickname ${nickname}`)
+
+    return newNodeData;
+}
+
+function NodeData_AquireLogStream()
+{
+    if (this.logStream != null && this.logStream.writeable)
+    {
+        this.logStream.end()
+    }
+
+    let dateCode = dateFormat(new Date(),"dd-mm-yy")
+    let logName = `log-${this.Nickname}-${dateCode}.txt`
+    let logPath = path.join(DHTAppConfig.LogDir, logName)
+
+    let fileIsNew = !fs.existsSync(logPath)
+    this.logStream = fs.createWriteStream(logPath, {flags:'a'})
+
+    if (fileIsNew)
+    {
+        this.logStream.write(`${dateFormat(new Date(),"dd/mm/yy @ hh:MM:ssTT")},\n`)
+        this.logStream.write(`time,temperature\n`)
+    }
+
+    let fileState = (fileIsNew) ? "created" : "opened"
+    console.log(`Aquired log file ${logName} (${fileState})`)
+}
 
 const app = express()
 
@@ -30,28 +85,43 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    
+    let lastReadings = []
+    for (const node of NodeRegistry)
+    {
+        let reading = node.DisplayGraphData[node.DisplayGraphData.length - 1]
+        lastReadings.push(reading)
+    }
+
     res.send(mustache.render(templates.index, {
-        currentTemp: tempatureDataSet[tempatureDataSet.length - 1].temp,
-        chartdata: tempatureDataSet,
+        Bedroom: FindNodeData("Bedroom").DisplayGraphData,
+        Mainroom: FindNodeData("Mainroom").DisplayGraphData,
+        NodeData: NodeRegistry,
+        Last: lastReadings
     }))
 })
 
 app.post('/log', (req, res) => {
-
+    
     let now = new Date()
+    let packet = req.body
+    // {"nick":"Mainroom","temp":26,"humi":44.9,"rssi":-41,"time":"04:06:01PM"}
 
-    let newdata = req.body
-    newdata.time = dateFormat(now, "hh:MM:ssTT")
-    console.log(JSON.stringify(newdata))
+    packet.time = now.getTime()
+    packet.log_time = dateFormat(now, "hh:MM:ssTT")
 
-    let logString = `${newdata.time},${newdata.temp}`
-    logStream.write(`${logString}\n`)
+    let nickname = packet.nick
+    let nodeData = FindNodeData(nickname, true)
 
-    newdata.time = now.getTime()
+    //nodeData.last_ip = req.connection.remoteAddress
 
-    tempatureDataSet.push(newdata)
-    while (tempatureDataSet.length > graphDataLength)
-        tempatureDataSet.shift()
+    let logString = `${packet.log_time},${packet.temp},${packet.humi},${packet.rssi}`
+    nodeData.logStream.write(`${logString}\n`)
+
+    let graphData = nodeData.DisplayGraphData
+    graphData.push(packet)
+    while (graphData.length > graphDataLength)
+        graphData.shift()
 
     res.send(`<pre>${dateFormat(now, "hh:MM:ss TT - dd/mm/yy")}</pre>`)
 })
@@ -74,34 +144,17 @@ app.get('/id', (req, res) => {
     res.send(`<pre>${JSON.stringify(machineInfo, undefined, 2)}</pre>`)
 })
 
-function SetLogStream()
-{
-    if (logStream != null && logStream.writeable)
-    {
-        logStream.end()
-    }
-
-    let dateCode = dateFormat(new Date(),"dd-mm-yy")
-    let logName = `log-${dateCode}.txt`
-    let logPath = path.join(DHTAppConfig.LogDir, logName)
-    logStream = fs.createWriteStream(logPath, {flags:'a'})
-
-    logStream.write(`${dateFormat(new Date(),"dd/mm/yy @ hh:MM:ssTT")},\n`)
-    logStream.write(`time,temperature\n`)
-
-    console.log(`Log file is now ${logName}`)
-    console.log(`Log rollover at ${dateFormat(new Date(), "hh:MM:ssTT - dd/mm/yy")}`)
-}
-
 app.listen(DHTAppConfig.HTTPPort, () => {
     
-
     if (!fs.existsSync(DHTAppConfig.LogDir))
         fs.mkdirSync(DHTAppConfig.LogDir);
 
-    SetLogStream();
     schedule.scheduleJob('0 0 * * *', () => {
-        SetLogStream();
+        console.log(`Log rollover at ${dateFormat(new Date(), "hh:MM:ssTT - dd/mm/yy")}`)
+        for (const node of NodeRegistry)
+        {
+            node.AquireLogStream()
+        }
     })
 
     console.log(`app listening on port ${DHTAppConfig.HTTPPort}`)
